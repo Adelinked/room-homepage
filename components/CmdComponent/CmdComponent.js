9671e1303844a58802830e72e604313daa51083e
Adel Aziz Boulfekhar <boulfekhar.adelaziz@gmail.com>
2022-09-20 20:36:15 +0100

    initial release

--- components/CmdComponent/CmdComponent.js
+++ components/CmdComponent/CmdComponent.js
@@ -3,11 +3,31 @@ import styles from "./CmdComponent.module.css";
 const CmdComponent = ({ handleClick }) => {
   return (
     <div className={styles.cmdComponentContainer}>
-      <div onClick={handleClick} id="left" className={styles.leftCmd}>
-        <Image src="/images/icon-angle-left.svg" height={24} width={14} />
+      <div
+        onClick={handleClick}
+        id="left"
+        className={styles.leftCmd}
+        tabIndex="6"
+      >
+        <Image
+          src="/images/icon-angle-left.svg"
+          height={24}
+          width={14}
+          alt="angle-left"
+        />
       </div>
-      <div onClick={handleClick} id="right" className={styles.rightCmd}>
-        <Image src="/images/icon-angle-right.svg" height={24} width={14} />
+      <div
+        onClick={handleClick}
+        id="right"
+        className={styles.rightCmd}
+        tabIndex="7"
+      >
+        <Image
+          src="/images/icon-angle-right.svg"
+          height={24}
+          width={14}
+          alt="angle-right  "
+        />
       </div>
     </div>
   );
